66f65946f16384bd9831fa0b719530d21cb39a2b
Michael John Peña <michael.pena@playtimesolutions.com.au>
2024-09-06 08:25:26 +1000

    More responsive search results and download

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,17 @@
-window.downloadAsPng = function (svgId) {
-    var svgElement = document.querySelector('#' + svgId);
+window.downloadAsPng = function (svgId, fileName) {
+    var svgElement = document.getElementById(svgId);
+    
+    if (!svgElement) {
+        console.error('SVG element not found:', svgId);
+        return;
+    }
 
     html2canvas(svgElement).then(function (canvas) {
         var link = document.createElement('a');
         link.href = canvas.toDataURL("image/png");
-        link.download = 'azure icon.png';
+        link.download = fileName + '.png';
         link.click();
+    }).catch(function(error) {
+        console.error('Error generating PNG:', error);
     });
-} 
+}
